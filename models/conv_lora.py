5aa17b75b3d63aafe2ce08207a5c3d9ec3fbf51d
imSkvor <im.skvor@yandex.ru>
2026-01-29 15:53:58 +0300

    CoLoRA layer, check weights initialization later

---
+++ models/conv_lora.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torch import Tensor
+
+class CoLoRALayer(nn.Module):
+    """
+    CoLoRA decomposes weight update into pointwise (1 x 1 x C) and layerwise (h x w x 1) components.
+    W = W_original + \delta W, where \delta W = pointwise_conv \circ layerwise_conv.
+    It finetunes by learning low-rank \delta W convolution kernels.
+    """
+    def __init__(self, original_conv: nn.Conv2d, rank: int = 4, alpha: int = 8) -> None:
+        super().__init__()
+        self.original_conv = original_conv
+        self.rank = rank
+        self.alpha = alpha
+
+        for param in self.original_conv.parameters():
+            param.requres_grad = False # freezeing original weights
+        
+        in_channels = original_conv.in_channels
+        out_channels = original_conv.out_channels
+        kernel_size = original_conv.kernel_size
+        groups = original_conv.groups
+
+        # CoLoRA decomposition
+        # pointwise 1x1
+        self.lora_pointwise = nn.Conv2d(
+            in_channels, out_channels,
+            kernel_size = 1, stride = 1, padding = 0,
+            groups = groups, bias = False
+        )
+
+        if isinstance(kernel_size, tuple):
+            kernel_size_tuple = kernel_size
+        else:
+            kernel_size_tuple = (kernel_size, kernel_size)
+
+        # layerwise
+        self.lora_layerwise = nn.Conv2d(
+            out_channels, out_channels,
+            kernel_size = kernel_size_tuple,
+            stride = original_conv.stride,
+            padding = original_conv.padding,
+            groups = out_channels,
+            bias = False
+        )
+
+        nn.init.kaiming_uniform_(self.lora_pointwise.weight, a = math.sqrt(5)) # <- check this against more articles?
+        nn.init.zeros_(self.lora_layerwise.weight)
+
+        self.scaling: float = alpha / rank
+
+    def forward(self, x: Tensor) -> Tensor:
+        original_output: Tensor = self.original_conv(x)
+        lora_output: Tensor = self.lora_layerwise(self.lora_pointwise(x))
+        return original_output + self.scaling * lora_output
+    
+    @property
+    def weight(self) -> Tensor:
+        with torch.no_grad():
+            pw_weight: Tensor = self.lora_pointwise.weight
+            lw_weight: Tensor = self.lora_layerwise.weight
+
+            combined: Tensor = self.scaling * lw_weight * pw_weight
+            return self.original_conv.weight + combined
+
+    def extra_repr(self) -> str:
+        return f"{self.rank=}, {self.alpha=}, {self.original_conv=}"
